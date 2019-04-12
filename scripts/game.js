632ef6fdc4695a3f60a063873a1752c7f31577d1
Anthony Jackman <anthony.jackman@serco-na.com>
2019-04-11 22:25:09 -0500

    adds screen scaling

--- scripts/game.js
+++ scripts/game.js
@@ -9,6 +9,11 @@ var GameState = {
     this.load.image('sheep', '../images/sheep3.png');
   },
   create: function() {
+
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
     this.background = this.game.add.sprite(0, 0, 'background');
     this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
     this.chicken.anchor.setTo(0.5);
