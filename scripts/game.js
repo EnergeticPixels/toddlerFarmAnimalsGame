306478c3de67b8f01ae48fd58c8ad087c9ed22ed
Anthony Jackman <anthony.jackman@serco-na.com>
2019-04-11 22:41:39 -0500

    adds user input

--- scripts/game.js
+++ scripts/game.js
@@ -7,6 +7,7 @@ var GameState = {
     this.load.image('horse', '../images/horse.png');
     this.load.image('pig', '../images/pig.png');
     this.load.image('sheep', '../images/sheep3.png');
+    this.load.image('arrow', '../images/arrow.png');
   },
   create: function() {
 
@@ -15,23 +16,46 @@ var GameState = {
     this.scale.pageAlignVertically = true;
 
     this.background = this.game.add.sprite(0, 0, 'background');
-    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-    this.chicken.anchor.setTo(0.5);
 
-    //this.chicken.scale.setTo();
-
-    this.pig = this.game.add.sprite(500, 300, 'pig' );
+    this.pig = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pig');
     this.pig.anchor.setTo(0.5);
-    this.pig.scale.setTo(-1, 1);
+    // enableuser input on sprite
+    this.pig.inputEnabled = true;
+    this.pig.input.pixelPerfectClick = true;
+    this.pig.events.onInputDown.add(this.animateAnimal, this);
 
-    this.sheep = this.game.add.sprite(100, 250, 'sheep');
-    this.sheep.scale.setTo(0.5);
-    this.sheep.anchor.setTo(0.5);
-    this.sheep.angle = -45;
+    // left arrow
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
+
+    // left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    // right arrow
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    // right arrow allow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
 
   },
   update: function() {
 
+  },
+
+  switchAnimal: function(sprite, event) {
+    console.log('move animal');
+  },
+
+  animateAnimal: function(sprite, event) {
+    console.log('anime animal')
   }
 };
 
