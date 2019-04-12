8e74cb1852705b549651d57e3598478851acdaa7
Anthony Jackman <anthony.jackman@serco-na.com>
2019-04-11 22:05:06 -0500

    adds chicken, changes anchor point

--- scripts/game.js
+++ scripts/game.js
@@ -3,9 +3,15 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO);
 var GameState = {
   preload: function( ) {
     this.load.image('background', '../images/background.png');
+    this.load.image('chicken', '../images/chicken.png');
+    this.load.image('horse', '../images/horse.png');
+    this.load.image('pig', '../images/pig.png');
+    this.load.image('sheep', '../images/sheep3.png');
   },
   create: function() {
     this.background = this.game.add.sprite(0, 0, 'background');
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    this.chicken.anchor.setTo(0.5);
   },
   update: function() {
 
