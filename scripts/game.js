542bb45f0f74d3308330fda2ba196cb6523f274d
Anthony Jackman <anthony.jackman@serco-na.com>
2019-04-14 13:49:06 -0500

    adds basic animal animation tweens

--- scripts/game.js
+++ scripts/game.js
@@ -83,13 +83,22 @@ var GameState = {
 
     if (sprite.customParams.direction > 0) {
       newAnimal = this.animals.next();
+      newAnimal.x = -newAnimal.width/2;
       endX = 640 + this.currentAnimal.width/2;
     } else {
       newAnimal = this.animals.previous();
+      newAnimal.x = 640 + newAnimal.width/2;
       endX = this.currentAnimal.width/2;
     }
-    this.currentAnimal.x = endX;
-    newAnimal.x = this.game.world.centerX;
+    
+    var newAnimalMovement = game.add.tween(newAnimal);
+    newAnimalMovement.to({ x: this.game.world.centerX }, 1000 );
+    newAnimalMovement.start();
+
+    var currentAnimalMovement = this.game.add.tween(this.currentAnimal);
+    currentAnimalMovement.to({ x: endX}, 1000 );
+    currentAnimalMovement.start();
+
     this.currentAnimal = newAnimal;
   }
 
